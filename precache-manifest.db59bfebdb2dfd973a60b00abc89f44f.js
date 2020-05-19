3f6ab4fd26e7ea22562975334120d413f41231be
Anand <anand.sg@fleximc.com>
2020-05-19 11:10:58 +0530

    Updates

---
+++ precache-manifest.db59bfebdb2dfd973a60b00abc89f44f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaaaf4497e1f0592956a29aac5008114",
+    "url": "/bankdatadeploy/index.html"
+  },
+  {
+    "revision": "de3c26886058d5052309",
+    "url": "/bankdatadeploy/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c39c6eba2f2e392cdf18",
+    "url": "/bankdatadeploy/static/css/main.74d58e95.chunk.css"
+  },
+  {
+    "revision": "de3c26886058d5052309",
+    "url": "/bankdatadeploy/static/js/2.be6cf2b4.chunk.js"
+  },
+  {
+    "revision": "3cabab53dee4769bc4a8c63b1d32918c",
+    "url": "/bankdatadeploy/static/js/2.be6cf2b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c39c6eba2f2e392cdf18",
+    "url": "/bankdatadeploy/static/js/main.27e4ba86.chunk.js"
+  },
+  {
+    "revision": "65cc0f5e9e1a9f69b86e",
+    "url": "/bankdatadeploy/static/js/runtime-main.6ed80ec4.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/bankdatadeploy/static/media/logo.ee7cd8ed.svg"
+  }
+]);
